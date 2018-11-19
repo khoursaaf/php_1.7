823ab337416196bb1843bc7204366af136eb3343
khoursaaf <khoursa@gmail.com>
2018-11-19 10:05:42 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Exercise</title>
+    </head>
+    <body>
+      <p>Créer trois variables lastname , firstname et age et les initialiser avec les valeurs de votre choix. Attention age est de type entier. Afficher : "Bonjour" + lastname + firstname + ",tu as" + age + "ans".</p>
+      <?php
+      $lastname = "Adoum Fadoul";
+      $firstname = "Khoursa";
+      $age = 21;
+      echo "Bonjour ".$firstname." ".$lastname.", tu as ".$age." ans.";
+      ?>
+    </body>
+</html>
