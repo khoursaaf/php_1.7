77530ad9474d3519139f79fdb7c90819a0e2a120
khoursaaf <khoursa@gmail.com>
2018-11-20 16:20:56 +0100

    correction

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Exercise</title>
-    </head>
-    <body>
-      <p>Créer trois variables lastname , firstname et age et les initialiser avec les valeurs de votre choix. Attention age est de type entier. Afficher : "Bonjour" + lastname + firstname + ",tu as" + age + "ans".</p>
-      <?php
-      $lastname = "Adoum Fadoul";
-      $firstname = "Khoursa";
-      $age = 21;
-      echo "Bonjour ".$firstname." ".$lastname.", tu as ".$age." ans.";
-      ?>
-    </body>
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercise</title>
+  </head>
+  <body>
+    <p>Créer trois variables lastname , firstname et age et les initialiser avec les valeurs de votre choix. Attention age est de type entier. Afficher : "Bonjour" + lastname + firstname + ",tu as" + age + "ans".</p>
+    <?php
+    $lastname = "Adoum Fadoul";
+    $firstname = "Khoursa";
+    $age = 21; ?>
+    <p><?php echo "Bonjour ".$firstname." ".$lastname.", tu as ".$age." ans.";
+    ?>
+    </p>
+  </body>
 </html>
